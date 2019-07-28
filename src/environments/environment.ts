8c04eb46368918e9104514c434a71350e0994205
Alina <alina.diezgr@gmail.com>
2019-07-28 13:16:38 -0500

    connected angular to firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA9PNPLKXL2wnYQN1kd7mrOEL9BYabkwjg",
+    authDomain: "sys-psychological-therapy.firebaseapp.com",
+    databaseURL: "https://sys-psychological-therapy.firebaseio.com",
+    projectId: "sys-psychological-therapy",
+    storageBucket: "sys-psychological-therapy.appspot.com",
+    messagingSenderId: "724575448212",
+    appId: "1:724575448212:web:501971587fd2bdae"
+  }
 };
 
 /*
