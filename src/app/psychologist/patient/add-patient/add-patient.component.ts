dff9b1122ede8c15b5c2d58ca6ed5bba8c858718
Alina <alina.diezgr@gmail.com>
2019-08-08 18:49:44 -0500

    Edited add-patient

--- src/app/psychologist/patient/add-patient/add-patient.component.ts
+++ src/app/psychologist/patient/add-patient/add-patient.component.ts
@@ -1,26 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder,  FormGroup, Validators} from '@angular/forms';
+import {PatientService} from '../../../services/patient.service';
 import {Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
 import {AuthService} from '../../../services/auth.service';
 
-@Component({
-  selector: 'app-add-user',
-  templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.css']
-})
-export class AddUserComponent implements OnInit {
 
-  usrForm: FormGroup;
+@Component({
+  selector: 'app-add-patient',
+  templateUrl: './add-patient.component.html',
+  styleUrls: ['./add-patient.component.css']
+})
+export class AddPatientComponent implements OnInit {
+
+  patientForm: FormGroup;
   authError: any;
-  roles = [
+  sexos = [
     {
       val: 1,
-      display: 'Psicologo'
+      display: 'Femenino'
     },
     {
       val: 2,
-      display: 'Secretaria'
+      display: 'Masculino'
+    }];
+  civils = [
+    {
+      val: 1,
+      display: 'Soltero'
+    },
+    {
+      val: 2,
+      display: 'Casado'
+    },
+    {
+      val: 3,
+      display: 'Divorciado'
+    },
+    {
+      val: 4,
+      display: 'Viudo'
+    }];
+  ses = [
+    {
+      val: 1,
+      display: 'Alto'
+    },
+    {
+      val: 2,
+      display: 'Medio'
+    },
+    {
+      val: 3,
+      display: 'Alto'
+    }];
+  scholaritys = [
+    {
+      val: 1,
+      display: 'Primaria'
+    },
+    {
+      val: 2,
+      display: 'Secundaria'
+    },
+    {
+      val: 3,
+      display: 'Preparatoria'
+    },
+    {
+      val: 4,
+      display: 'Licenciatura'
     }];
 
   constructor(private authServ: AuthService,
@@ -36,27 +85,56 @@ export class AddUserComponent implements OnInit {
   }
 
   createForm() {
-    this.usrForm = this.fb.group({
+    this.patientForm = this.fb.group({
       name: ['', Validators.required],
-      email: ['', Validators.required],
-      pwd: ['', Validators.required],
-      role: ['', Validators.required]
+      age: ['', Validators.required],
+      sexo: ['', Validators.required],
+      civil: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      hometown: ['', Validators.required],
+      address: ['', Validators.required],
+      homephone: ['', Validators.required],
+      cellphone: ['', Validators.required],
+      ocupation: ['', Validators.required],
+      scholarity: ['', Validators.required],
+      se: ['', Validators.required],
+      religion: ['', Validators.required],
+      reference: ['', Validators.required],
+      guardian: ['', Validators.required],
+      source: ['', Validators.required],
+      diagnostic: ['', Validators.required],
+      pronostic: ['', Validators.required]
     });
   }
 
   resetFields() {
-    this.usrForm = this.fb.group({
-      name: ['', [Validators.required]],
-      email: ['', Validators.required],
-      pwd: ['', Validators.required],
-      role: ['', Validators.required]
+    this.patientForm = this.fb.group({
+      name: ['', Validators.required],
+      age: ['', Validators.required],
+      sexo: ['', Validators.required],
+      civil: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      hometown: ['', Validators.required],
+      address: ['', Validators.required],
+      homephone: ['', Validators.required],
+      cellphone: ['', Validators.required],
+      ocupation: ['', Validators.required],
+      scholarity: ['', Validators.required],
+      se: ['', Validators.required],
+      religion: ['', Validators.required],
+      reference: ['', Validators.required],
+      guardian: ['', Validators.required],
+      source: ['', Validators.required],
+      diagnostic: ['', Validators.required],
+      pronostic: ['', Validators.required]
     });
   }
 
   onSubmit(value) {
-    this.authServ.createAdminUser(value).then(res => {
+    this.authServ.createPatientUser(value).then(res => {
       if (res === true) {
         this.onStatus('', res);
+        this.router.navigate(['users']);
       } else {
         this.onStatus(this.authError.message, false);
       }
@@ -66,10 +144,9 @@ export class AddUserComponent implements OnInit {
 
   onStatus(message, val: boolean) {
     if (val === true) {
-      this.toastr.success('Se agregó el usuario con éxito', 'Éxito');
+      this.toastr.success('Se agregó correctamente al paciente', 'Éxito');
     } else {
       this.toastr.error(message, 'Ha ocurrido un error');
     }
   }
-
 }
