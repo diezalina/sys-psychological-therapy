1970ff31ed54b1d50dedb8e311ba0164ab785443
Alina <alina.diezgr@gmail.com>
2019-08-08 13:42:31 -0500

    Updated app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import {UsersComponent} from './psychologist/users/users/users.component';
 import {PatientComponent} from './psychologist/patient/patient/patient.component';
+import {LoginComponent} from './auth/login/login.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'patient',
     component: PatientComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '**',
